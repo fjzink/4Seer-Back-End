6e942649ffce27f7ee5eebe0771346224ae79dc7
peanutenthusiast <mhdedios@gmail.com>
2017-10-08 07:27:00 -0700

    Refactor cors.rb file, re-add rack-cors gem

--- config/application.rb
+++ config/application.rb
@@ -28,11 +28,13 @@ module ForeSeerBackEnd
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
 
     config.action_dispatch.default_headers = {
       'Access-Control-Allow-Origin' => '*',
       'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
     }
+    
+    config.api_only = true
+
   end
 end
